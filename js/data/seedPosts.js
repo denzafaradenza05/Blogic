ea613ba7f1f8327f8270bac16fb4cb6e049501a4
denzafaradenza05 <164926885+denzafaradenza05@users.noreply.github.com>
2026-06-19 16:37:32 +0500

    Add files via upload

---
+++ js/data/seedPosts.js
@@ -0,0 +1,54 @@
+export const seedPosts = [
+    {
+        id: 1,
+        userId: 1,
+        text: 'ПрИвЕт ВсЕм! Это мой первый пост в Blogic. Рад быть частью этого сообщества!',
+        date: '2026-06-15T10:30:00',
+        likes: 5,
+        likedByMe: false,
+        views: 127,  
+        comments: []
+    },
+    {
+        id: 2,
+        userId: 2,
+        image: 'images/post1.jpg',
+        text: 'Сегодня прекрасная погода для прогулки! Кто со мной? ☀️',
+        date: '2026-06-16T14:20:00',
+        likes: 12,
+        likedByMe: false,
+        views: 342, 
+        comments: []
+    },
+    {
+        id: 3,
+        userId: 3,
+        text: 'Закончил строительство дома. Делюсь впечатлениями в следующем посте!',
+        date: '2026-06-17T09:15:00',
+        likes: 8,
+        likedByMe: false,
+        views: 215, 
+        comments: []
+    },
+    {
+        id: 4,
+        userId: 1,
+        text: 'В разводе всегда виноваты оба - и жена и тёща',
+        date: '2026-06-18T16:45:00',
+        likes: 3,
+        likedByMe: false,
+        views: 89,  
+        comments: []
+    },
+    {
+        id: 5,
+        userId: 4,
+        image: 'images/post2.jpg',
+        text: 'Только что вернулась из путешествия. Столько впечатлений!',
+        date: '2026-06-19T11:00:00',
+        likes: 15,
+        likedByMe: false,
+        views: 456, 
+        comments: []
+    }
+];
